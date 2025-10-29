d7932a1cd9fe2d23b29658e965558dc13339287d
Marianna Taavitsainen <marianna.taavitsainen@gmail.com>
2025-10-29 19:48:09 +0200

    kokeilu2

---
+++ H1_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout << "Anna nimesi: " << endl;
+    cin>> name;
+    cout << "Anna ikasi: ";
+    cin >> age;
+    cout << " Terve " << name << ". Olet siis " << age << "-vuotias" << endl;
+    return 0;
+}
